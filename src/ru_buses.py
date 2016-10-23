d6ef260369e61d17b8037d24fe1dcc9b6da2e586
parth <prparikh02@gmail.com>
2016-10-23 10:54:55 -0400

    Support for invalid query and route not running responses Added a few more mappings for bus routes

--- src/ru_buses.py
+++ src/ru_buses.py
@@ -19,6 +19,44 @@ def get_welcome_response():
     return build_response(attributes, build_speechlet_response(card_title, speech_output, reprompt_text, end_session))
 
 
+def invalid_query_response(r, s):
+
+    attributes = {}
+    card_title = "Invalid Query!"
+    speech_output = "The %s route does not stop at %s" % (r, s)
+    reprompt_text = "Please ask for a valid route prediction"
+    end_session = False
+
+    return build_response(attributes, build_speechlet_response(card_title, speech_output, reprompt_text, end_session))
+
+
+def route_not_running_response(r):
+
+    attributes = {}
+    card_title = "The %s Route is not Running!" % r
+    speech_output = "It seems like the %s route is currently not running" % r
+    reprompt_text = "Please ask for another route prediction"
+    end_session = False
+
+    return build_response(attributes, build_speechlet_response(card_title, speech_output, reprompt_text, end_session))
+
+
+# TODO: List off all places of potential conflict and read them back to user
+def resolve_proper_stop():
+
+    # For example, the EE stops at train station twice, (traine & trainn_a)
+    #   We would list in app and read aloud very specific cases:
+    #   "The EE is arriving at train station northbound in __ mins & southbound in ___ mins"
+
+    attributes = {}
+    card_title = "Potentially Ambiguous Query"
+    speech_output = ""
+    reprompt_text = None
+    end_session = False
+
+    return build_response(attributes, build_speechlet_response(card_title, speech_output, reprompt_text, end_session))
+
+
 def fetch_bus_arrival_predictions(intent, session):
 
     session_attributes = {}
@@ -38,11 +76,9 @@ def fetch_bus_arrival_predictions(intent, session):
     target_stop = set(s_enc).intersection(set(r_stops))
 
     if len(target_stop) == 0:
-        print 'we gon do something later'
-        # invalid_query_response()
+        return invalid_query_response(r, s)
     elif len(target_stop) == 2:
-        print 'we gon do something else later'
-        # resolve_proper_stop()
+        resolve_proper_stop()
 
     (target_stop,) = target_stop
     prediction_payload = {
@@ -58,18 +94,18 @@ def fetch_bus_arrival_predictions(intent, session):
     try:
         arrival_in_mins = int(root.iter('prediction').next().get('minutes'))
     except StopIteration:
-        print 'route not running'
-        # route_not_running_response()
-
+        return route_not_running_response(r)
 
     speech_output = "The %s arrives at %s in %d minutes" % (r, s,  arrival_in_mins)
+    reprompt_text = None
+    end_session = False
 
-    return build_response(session_attributes, build_speechlet_response(intent['name'], speech_output, None, False))
+    return build_response(session_attributes, build_speechlet_response(intent['name'], speech_output, reprompt_text, end_session))
 
 # -- Response Helpers --
 
 
-def build_speechlet_response(title, output, reprompt_text, end_session):
+def build_speechlet_response(title, output, reprompt_text, end_session=False):
 
     return {
         'outputSpeech': {
@@ -78,8 +114,8 @@ def build_speechlet_response(title, output, reprompt_text, end_session):
         },
         'card': {
             'type': 'Simple',
-            'title': "SessionSpeechlet - " + title,
-            'content': "SessionSpeechlet - " + output
+            'title': title,
+            'content': output
         },
         'reprompt': {
             'outputSpeech': {
